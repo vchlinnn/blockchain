d370501ab036c21d81f4098748f046befc83436a
vchlinnn <chuclinh190305@gmail.com>
2025-03-01 20:50:29 -0600

    Implement Hash object

--- src/main/java/edu/grinnell/csc207/blockchain/Hash.java
+++ src/main/java/edu/grinnell/csc207/blockchain/Hash.java
@@ -4,5 +4,33 @@ package edu.grinnell.csc207.blockchain;
  * A wrapper class over a hash value (a byte array).
  */
 public class Hash {
-    // TODO: fill me in!
+    private byte[] hashData;
+
+    public Hash(byte[] data) {
+        this.hashData = data;
+    }
+
+    public byte[] getData() {
+        return hashData;
+    }
+
+    public boolean isValid() {
+        return hashData[0] == 0 && hashData[1] == 0 && hashData[2] == 0;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < hashData.length; i++) {
+            sb.append(String.format("%02x", Byte.toUnsignedInt(hashData[i])));
+        }
+        return sb.toString();
+    }
+
+    public boolean equals(Object other) {
+        if (other instanceof Hash) {
+            Hash o = (Hash) other;
+            return hashData.equals(o.getData());
+        }
+        return false;
+    }
 }
