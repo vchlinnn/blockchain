9aef537a3d7dd24b9d6d6be682ed37e4ec8bd778
vchlinnn <chuclinh190305@gmail.com>
2025-03-02 10:54:53 -0600

    Implement blockchain constructor

--- src/main/java/edu/grinnell/csc207/blockchain/BlockChain.java
+++ src/main/java/edu/grinnell/csc207/blockchain/BlockChain.java
@@ -1,9 +1,31 @@
 package edu.grinnell.csc207.blockchain;
 
+import java.security.NoSuchAlgorithmException;
+
 /**
  * A linked list of hash-consistent blocks representing a ledger of
  * monetary transactions.
  */
 public class BlockChain {
-    // TODO: fill me in!
+
+    private Node first;
+    private Node last;
+    private int size;
+
+    private static class Node {
+        Block block;
+        Node next;
+
+        Node(Block blockValue) {
+            this.block = blockValue;
+            this.next = null;
+        }
+    }
+
+    public BlockChain(int initial) throws NoSuchAlgorithmException {
+        Block firstBlock = new Block(0, initial, null); // prevHash should be ignored
+        this.first = new Node(firstBlock);
+        this.last = this.first;
+        this.size = 1;
+    }
 }
